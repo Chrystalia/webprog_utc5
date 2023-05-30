7396274b806d31fc44fb755b468fad2a0ec26876
Chrystalia <glenysang@gmail.com>
2023-05-30 11:58:48 +0700

    Final

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    {{-- tilwind cdn, for styling --}}
+    {{-- tailwind cdn, for styling --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 
